c0e5086955780f29fd8f0673215f0b6696768b9a
Tetiana Bahlai <tania.baglay@gmail.com>
2023-01-02 16:42:02 +0200

    Implemented Onboarding Transformation Screen

---
+++ app/src/main/java/com/tbahlai/cryptowallet/presentation/onboarding/transformation/OnboardingTransformationFragment.kt
@@ -0,0 +1,10 @@
+package com.tbahlai.cryptowallet.presentation.onboarding.transformation
+
+import androidx.fragment.app.viewModels
+import com.tbahlai.cryptowallet.R
+import com.tbahlai.cryptowallet.common.BaseFragment
+
+class OnboardingTransformationFragment : BaseFragment(R.layout.fragment_onboarding_transformation) {
+
+    private val viewModel by viewModels<OnboardingTransformationViewModel>()
+}
