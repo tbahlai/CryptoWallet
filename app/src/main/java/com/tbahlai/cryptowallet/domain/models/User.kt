a9355697a4aa93fd8d3ae22e5dfe88775cd90a6d
tbahlai <45667702+tbahlai@users.noreply.github.com>
2023-01-04 14:07:08 +0200

    Revert "Profile screen (#5)" (#6)
    This reverts commit 9f2ebf98b4aa0a96620f950cc40b9b07292cf98d.

--- app/src/main/java/com/tbahlai/cryptowallet/domain/models/User.kt
+++
@@ -1,8 +0,0 @@
-package com.tbahlai.cryptowallet.domain.models
-
-data class User(
-    val id: Long = 0L,
-    val name: String = "Matthew",
-    val imageUrl: String = "https://thispersondoesnotexist.com/image",
-    val currentBalance: Double = 246.0
-)
